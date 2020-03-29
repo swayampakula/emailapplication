22912aac7a2b47b01c296e58814df43e7ef66a4d
Ramesh N (Digital) <RA353444@wipro.com>
2020-03-29 19:31:46 +0530

    commented code for redux-persist

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Container from "./components/Container";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+// import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
   return (
     <div>
       <Provider store={store()}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
           <Switch>
             <Route path="/inbox" component={Container} />
@@ -16,6 +18,7 @@ function App() {
             <Redirect to="/inbox" exact />
           </Switch>
         </BrowserRouter>
+        {/* </PersistGate> */}
       </Provider>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,25 @@ import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { inboxMails } from "./reducers/inboxreducer";
 
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// }
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+// let persistor = persistStore(store)
+
 const store = () => {
   const store = createStore(
     combineReducers({ inboxMails }),
     composeWithDevTools(applyMiddleware(thunk, logger))
   );
+  // let persistor = persistStore(store);
   return store;
+  // return {store , persistor}
 };
 
 export default store;
